12baf3e910047f8968b703b76c461ac5615715e3
Shresth <shresth.chomal@ascendion.com>
2025-05-10 03:10:40 +0530

    Added Migrations

--- profiles_project/profiles_api/models.py
+++ profiles_project/profiles_api/models.py
@@ -1,8 +1,46 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser
 from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.models import BaseUserManager
+
 
 # Create your models here.
+
+class UserProfileManager(BaseUserManager):
+    """Managers for User Profile"""
+    """Can Manipulate objects and return that object"""
+
+    def create_user(self, name,email, password=None):
+        if not email:
+            raise ValueError('User Must have an email address')
+        
+        if not name:
+            raise ValueError("User must enetr a user name")
+        
+        if not password:
+            raise ValueError('User must enter a password')
+        
+        email = self.normalize_email(email=email)
+        user = self.model(email=email,name=name)
+
+        user.set_password(password)
+        user.save(using = self._db)
+
+        return user
+    
+    def create_superuser(self,name,email,password=None):
+        """Creating super user"""
+
+        user = self.create_user(name=name,email=email,password=password)
+
+        user.is_superuser = True
+        user.is_staff = True
+
+        user.save(using = self._db)
+        return user
+
+
+
 
 class UserProfile(AbstractBaseUser, PermissionsMixin):
     """Database Model for users in the system"""
