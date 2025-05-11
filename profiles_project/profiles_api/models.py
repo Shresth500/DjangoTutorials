e8a6d30989bd58adcd3c6092a1117555656dd8e4
Shresth <shresth.chomal@ascendion.com>
2025-05-12 01:43:34 +0530

    Added Profile Feed Model and api end points

--- profiles_project/profiles_api/models.py
+++ profiles_project/profiles_api/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser
 from django.contrib.auth.models import PermissionsMixin
 from django.contrib.auth.models import BaseUserManager
+from profiles_project import settings
 
 
 # Create your models here.
@@ -68,3 +69,16 @@ class UserProfile(AbstractBaseUser, PermissionsMixin):
     # convert a user profile object to a string in python
     def __str__(self):
         return self.email
+    
+
+class ProfileFeedItem(models.Model):
+    user_profile = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+
+    status_text = models.CharField(max_length=255)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.status_text
